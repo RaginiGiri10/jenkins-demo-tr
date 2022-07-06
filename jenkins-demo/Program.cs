375e9961ea42ad5c721d779070850e0bb8e4d0a3
RAGINIGIRI10 <raginigiri10@gmail.com>
2022-07-06 05:56:27 -0700

    InitialCommit

---
+++ jenkins-demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace jenkins_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Checking jenkins build
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
